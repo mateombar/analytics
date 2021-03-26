eb04a5695f06c93ff34bf2a684174ddf203f2fc5
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-03-26 16:59:03 -0500

    Navbar Layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { Context } from "../context/Context";
 import EditableElement from "./EditableElement";
+import "./styles/Navbar.css";
+import logo from "../assets/svg/logo.svg";
 export const Navbar = () => {
   const {
     state: {
@@ -10,14 +12,69 @@ export const Navbar = () => {
     setActualTextContent,
   } = useContext(Context);
   return (
-    <nav>
-      <EditableElement
-        isContentEditable={isContentEditable}
-        onBlur={setActualTextContent}
-        key_name="navbar.a_products"
-      >
-        <a href="/">{navbar.a_products}</a>
-      </EditableElement>
+    <nav className="navbar">
+      <img className="navbar__logo" src={logo} alt="Logo" />
+      <ul className="navbar__links">
+        <li className="navbar__links--link">
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="navbar.a_products"
+          >
+            <a href="/">
+              {navbar.a_products}
+            </a>
+          </EditableElement>
+        </li>
+        <li className="navbar__links--link">
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="navbar.a_pricing"
+          >
+            <a href="/">
+              {navbar.a_pricing}
+            </a>
+          </EditableElement>
+        </li>
+        <li className="navbar__links--link">
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="navbar.a_faq"
+          >
+            <a href="/">
+              {navbar.a_faq}
+            </a>
+          </EditableElement>
+        </li>
+        <li className="navbar__links--link">
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="navbar.a_blog"
+          >
+            <a href="/">
+              {navbar.a_blog}
+            </a>
+          </EditableElement>
+        </li>
+        <li className="navbar__links--link">
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="navbar.a_blog"
+          >
+            <a href="/">
+              {navbar.a_blog}
+            </a>
+          </EditableElement>
+        </li>
+      </ul>
+      <div className="navbar__buttons">
+        <a className="navbar__buttons--link">{navbar.btn_sign_ing}</a>
+        <button className="navbar__buttons--btn">{navbar.btn_sign_up}</button>
+      </div>
     </nav>
   );
 };
