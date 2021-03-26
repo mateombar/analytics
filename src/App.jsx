a77eb9cef4921fb36af25a8f33a51499d22bd681
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-03-26 10:45:09 -0500

    Adding setTextContent and toggleContentEditable to useContext

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Fold } from "./components/Fold";
+import { Navbar } from "./components/Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Fold></Fold>
+      <main></main>
+    </>
+  );
+}
+
+export default App;
--- src/hooks/useContext.js
+++ src/hooks/useContext.js
@@ -1,7 +1,47 @@
-import {useEffect, useState} from 'react';
-
-export const useContext = () =>{
+import { useEffect, useState } from 'react';
+import initialState from '../context/initialState.json';
+import txtTags from '../context/txtTags.json';
+export const useContext = () => {
     const [state, setState] = useState({});
 
-    return {state};
+    useEffect(() => {
+        const content = JSON.parse(window.localStorage.getItem('txtTags'));
+        if (content) {
+            setState({
+                initialState,
+                tagContent: content
+            })
+        } else {
+            setState({
+                initialState,
+                tagContent: txtTags
+            })
+        }
+    }, [])
+
+    const setTextContent = (key_name, value) => {
+        const arr_name = key_name.split('.', 2);
+        const component_name = arr_name[0].toString();
+        const k_name = arr_name[1].toString();
+
+        const tagContent = { ...state.tagContent };
+        const component = { ...tagContent[component_name] };
+        component[k_name] = value;
+        tagContent[component_name] = component;
+        setState({
+            ...state,
+            tagContent
+        });
+        window.localStorage.setItem('txtTags', JSON.stringify(tagContent));
+    }
+    const toggleContentEditable = () => {
+        const initialState = { ...state.initialState, }
+        initialState.isContentEditable = !initialState.isContentEditable
+        setState({
+            ...state,
+            initialState
+        })
+    }
+
+    return { setTextContent, toggleContentEditable, state };
 }
