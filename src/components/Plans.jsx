4e75093f5f6a979e48a13f603518eda46647bf95
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-03-27 18:39:54 -0500

    Plans Styled

--- src/components/Plans.jsx
+++ src/components/Plans.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Context } from "../context/Context";
 import EditableElement from "./EditableElement";
+import "./styles/Plans.css";
 export const Plans = () => {
   const {
     state: {
@@ -27,6 +28,128 @@ export const Plans = () => {
           <p className="plans__description">{plans.p_presentation}</p>
         </EditableElement>
       </article>
+      <div className="plans__card">
+        <article className="card">
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="plans.h2_title_card_1"
+          >
+            <h2 className="card__title">{plans.h2_title_card_1}</h2>
+          </EditableElement>
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="plans.p_users_card_1"
+          >
+            <p className="card__users">{plans.p_users_card_1}</p>
+          </EditableElement>
+          <hr />
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="plans.h1_number_card_1"
+          >
+            <h1 className="card__number">{plans.h1_number_card_1}</h1>
+          </EditableElement>
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="plans.p_number_card_1"
+          >
+            <p className="card__p-number">{plans.p_number_card_1}</p>
+          </EditableElement>
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="plans.btn_card_1"
+          >
+            <button type="button" className="card__btn">
+              {plans.btn_card_1}
+            </button>
+          </EditableElement>
+        </article>
+        <article className="card card--white">
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="plans.h2_title_card_2"
+          >
+            <h2 className="card__title">{plans.h2_title_card_2}</h2>
+          </EditableElement>
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="plans.p_users_card_2"
+          >
+            <p className="card__users">{plans.p_users_card_2}</p>
+          </EditableElement>
+          <hr />
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="plans.h1_number_card_2"
+          >
+            <h1 className="card__number">{plans.h1_number_card_2}</h1>
+          </EditableElement>
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="plans.p_number_card_2"
+          >
+            <p className="card__p-number">{plans.p_number_card_2}</p>
+          </EditableElement>
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="plans.btn_card_2"
+          >
+            <button type="button" className="card__btn card__btn--pink">
+              {plans.btn_card_2}
+            </button>
+          </EditableElement>
+        </article>
+        <article className="card">
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="plans.h2_title_card_3"
+          >
+            <h2 className="card__title">{plans.h2_title_card_3}</h2>
+          </EditableElement>
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="plans.p_users_card_3"
+          >
+            <p className="card__users">{plans.p_users_card_3}</p>
+          </EditableElement>
+          <hr />
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="plans.h1_number_card_3"
+          >
+            <h1 className="card__number">{plans.h1_number_card_3}</h1>
+          </EditableElement>
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="plans.p_number_card_3"
+          >
+            <p className="card__p-number">{plans.p_number_card_3}</p>
+          </EditableElement>
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="plans.btn_card_3"
+          >
+            <button type="button" className="card__btn">
+              {plans.btn_card_3}
+            </button>
+          </EditableElement>
+        </article>
+      </div>
     </section>
   );
 };
