665c63b0bf28dd1b6209bc46ed4a18ec56bc87ab
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-03-26 23:54:50 -0500

    Styling navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../context/Context";
 import EditableElement from "./EditableElement";
 import "./styles/Navbar.css";
 import logo from "../assets/svg/logo.svg";
+import { Hamburger } from "./svg/Hamburger";
 export const Navbar = () => {
+  const [active, setActive] = useState(false);
   const {
     state: {
       initialState: { isContentEditable },
@@ -11,70 +13,80 @@ export const Navbar = () => {
     },
     setActualTextContent,
   } = useContext(Context);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
   return (
-    <nav className="navbar">
-      <img className="navbar__logo" src={logo} alt="Logo" />
-      <ul className="navbar__links">
-        <li className="navbar__links--link">
+    <>
+      <nav className={active ? "navbar active" : "navbar"}>
+        <img
+          className="navbar__logo"
+          src={logo}
+          alt="Logo"
+          onClick={handleClick}
+        />
+        <div className="navbar__links">
           <EditableElement
             isContentEditable={isContentEditable}
             onBlur={setActualTextContent}
             key_name="navbar.a_products"
           >
-            <a href="/">
-              {navbar.a_products}
-            </a>
+            <a href="/">{navbar.a_products}</a>
           </EditableElement>
-        </li>
-        <li className="navbar__links--link">
           <EditableElement
             isContentEditable={isContentEditable}
             onBlur={setActualTextContent}
             key_name="navbar.a_pricing"
           >
-            <a href="/">
-              {navbar.a_pricing}
-            </a>
+            <a href="/">{navbar.a_pricing}</a>
           </EditableElement>
-        </li>
-        <li className="navbar__links--link">
           <EditableElement
             isContentEditable={isContentEditable}
             onBlur={setActualTextContent}
             key_name="navbar.a_faq"
           >
-            <a href="/">
-              {navbar.a_faq}
-            </a>
+            <a href="/">{navbar.a_faq}</a>
           </EditableElement>
-        </li>
-        <li className="navbar__links--link">
           <EditableElement
             isContentEditable={isContentEditable}
             onBlur={setActualTextContent}
             key_name="navbar.a_blog"
           >
-            <a href="/">
-              {navbar.a_blog}
-            </a>
+            <a href="/">{navbar.a_blog}</a>
           </EditableElement>
-        </li>
-        <li className="navbar__links--link">
           <EditableElement
             isContentEditable={isContentEditable}
             onBlur={setActualTextContent}
             key_name="navbar.a_blog"
           >
-            <a href="/">
-              {navbar.a_blog}
+            <a href="/">{navbar.a_blog}</a>
+          </EditableElement>
+        </div>
+        <div className="navbar__buttons">
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="navbar.btn_sign_ing"
+          >
+            <a href="/" className="navbar__buttons--link">
+              {navbar.btn_sign_ing}
             </a>
           </EditableElement>
-        </li>
-      </ul>
-      <div className="navbar__buttons">
-        <a className="navbar__buttons--link">{navbar.btn_sign_ing}</a>
-        <button className="navbar__buttons--btn">{navbar.btn_sign_up}</button>
-      </div>
-    </nav>
+          <EditableElement
+            isContentEditable={isContentEditable}
+            onBlur={setActualTextContent}
+            key_name="navbar.btn_sign_up"
+          >
+            <button className="navbar__buttons--btn">
+              {navbar.btn_sign_up}
+            </button>
+          </EditableElement>
+        </div>
+        <button className="navbar__menu" onClick={handleClick}>
+          <Hamburger />
+        </button>
+      </nav>
+    </>
   );
 };
